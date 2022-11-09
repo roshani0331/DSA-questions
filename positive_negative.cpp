17a1ccfa982a7884d0b664af337c22b59a30d9d8
Roshani soni <76997030+roshani0331@users.noreply.github.com>
2022-11-09 20:05:09 +0530

    Add files via upload

---
+++ positive_negative.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+
+    if(n>0){
+        cout<<"positive number"<<endl;
+    }
+    else if(n<0){
+        cout<<"negative number"<<endl;
+    }
+    else
+         cout<<"zero"<<endl;
+
+}
